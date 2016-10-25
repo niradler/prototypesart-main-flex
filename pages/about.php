b7a02670ed9dc0f25aa13503c218dbdc06a61e82
nir <niradler55@gmail.com>
2016-10-26 01:18:41 +0300

    add content

--- pages/about.php
+++ pages/about.php
@@ -1,10 +1,78 @@
     <section id="about" class="section  notification is-info">
     <div class="container">
+
       <div class="heading">
         <h1 class="title">About</h1>
         <h2 class="subtitle">
-          A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+          we are a team of hardware and software developers.
         </h2>
+        <br>
+        </div>
+         <div class="columns">
+  <div class="column">
+        <div class="card">
+  <div class="card-image">
+    <figure class="image ">
+      <img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAASLAAAAJDBlZjUzNGMyLTViYzYtNDhkYS1iZTEzLWNhZWQ4MDM0M2ExYg.jpg" alt="">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-32x32">
+          <img src="http://www.pd4pic.com/images/parking-p-letter-symbol.png" alt="Image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-5">Nir Adler</p>
+        <p class="subtitle is-6">niradler55@gmail.com</p>
+      </div>
+    </div>
+
+    <div class="content">
+      <h3>Skills History </h3>
+
+  <p>dslkfjdskljflkdsjkl</p>
+      <br>
+      <small class="has-text-center">CEO</small>
+    </div>
+  </div>
+</div>
+</div>
+<div class="column">
+    <div class="content">
+      <h2 class="title">Skills History </h2>
+
+<ul>
+  <li>
+    Angular - deep knowledge of the framework, including: mvc pattern, routes, custom directives and services, working with mobile frameworks (Ionic) and other external services such as Firebase with AngularFire.
+  </li>
+  <li>
+    JQuery – Experienced with different jQuery tools and libraries, including Ajax.
+  </li>
+  <li>
+    HTML5, CSS3, Bootstrap and Material design, responsive design for different screen sizes optimising.
+  </li>
+  <li>
+    PHP – deep knowledge of the language including OOP design and working with databases (Laravel, Code ignitor, MySQL).
+  </li>
+  <li> 
+Python – Working on small projects, main focus on security tools and networking with Flask (ex. working with Pyhook for creating key logger etc.)
+  </li>
+  <li>
+    Embedded programing: C/C++ programing different types of microcontrollers (ex. Atmel, TI etc.).
+  </li>
+   <li>
+    Networking and protocol understanding, experienced in Wireshark and other Linux based tools (ex. Kali / Backtrack).
+  </li>
+    <li>
+    Programing University courses: C#, Java, C.
+  </li>
+</ul>
+      <br>
+      <small class="has-text-center">CEO</small>
+    </div>
+</div>
       </div>
     </div>
   </section>
--- pages/contact.php
+++ pages/contact.php
@@ -3,38 +3,40 @@
     <div class="container">
       <div class="heading">
         <h1 class="title">Contact Us</h1>
-        <h2 class="subtitle">
+      <!--   <h2 class="subtitle">
           for more info contact us 
-        </h2>
+        </h2> -->
       </div>
+      <div class="columns ">
+       <div class="column is-5">
+       <div class="content">
+         Any one with and idea, or a technical problem can use our services, from planing the product to produce it, we can suggest the hardware and software we recommend you to use, and help you push an idea to become a product.
+         send us a message for more details
+       </div>
+ </div>
+      <div class="column is-7">
       <div class="box">
   <article class="media">
 <form>
  <div class="columns ">
- <div class="column is-7">
+ <div class="column is-6">
   <p class="control has-icon">
-  <input class="input" type="email" placeholder="Email">
-  <i class="fa fa-envelope"></i>
+  <input class="input" type="text" placeholder="Name" required="">
+ <i class="fa fa-user"></i>
 </p>
 </div>
-<div class="column is-7">
+<div class="column is-6">
 <p class="control has-icon">
-  <input class="input" type="password" placeholder="Password">
-  <i class="fa fa-lock"></i>
+   <input class="input" type="email" placeholder="Email" required>
+  <i class="fa fa-envelope"></i>
 </p>
 </div>
 </div>
  <div class="columns ">
- <div class="column is-7">
+ <div class="column is-12">
   <p class="control has-icon">
-  <input class="input" type="email" placeholder="Email">
-  <i class="fa fa-envelope"></i>
-</p>
-</div>
-<div class="column is-7">
-<p class="control has-icon">
-  <input class="input" type="password" placeholder="Password">
-  <i class="fa fa-lock"></i>
+    <textarea class="textarea" placeholder="message" required></textarea>
+
 </p>
 </div>
 </div>
@@ -51,4 +53,6 @@
   </article>
 </div>
     </div>
+    </div>
+
   </section>
